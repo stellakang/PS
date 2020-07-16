a185651379d1e471385a64fd1a34d5c80ecc6d80
kangsj123 <kangsj123@naver.com>
2020-07-17 03:56:36 +0900

    update #383 code

--- easy/383_ransom_note.cpp
+++ easy/383_ransom_note.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
+        int mlen=magazine.length();
+        int rlen=ransomNote.length();
         unordered_map<char,int>cnt;
-        for(int i=0;i<magazine.length();i++)
+        for(int i=0;i<mlen;i++)
             cnt[magazine[i]]++;
-        for(int i=0;i<ransomNote.length();i++){
+        for(int i=0;i<rlen;i++){
             char c=ransomNote[i];
             if(cnt[c]==0)
                 return false;
