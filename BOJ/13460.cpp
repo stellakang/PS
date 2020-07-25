ae77bbae50c061c63b89dda47992cb4b3b686e7e
kangsj123 <kangsj123@naver.com>
2020-07-26 03:17:48 +0900

    fix code

--- BOJ/13460.cpp
+++ BOJ/13460.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 struct Position{
-    int xpos,ypos;
+    int x,y;
 };
 
-int move(int N, int M, Position r, Position b, int iters, int direction);
+int moveBeads(int N, int M, Position r, Position b, int iters, int direction);
 void updatePosition(int &x, int &y, int xmove, int ymove);
 bool isEqual(int x1, int y1, int x2, int y2);
 
@@ -13,40 +13,32 @@ char** board;
 
 int main(int argc, const char * argv[]) {
     int N,M;
-    scanf("%d%d",&N,&M);
+    cin>>N>>M;
     
     board = new char*[N];
     for(int i=0;i<N;i++)
         board[i]=new char[M];
     
-    char tmp;
-    scanf("%c",&tmp);
-    
     Position r,b;
     for(int i=0;i<N;i++){
-        scanf("%s",board[i]);
+        cin>>board[i];
         for(int j=0;j<M;j++){
-            if(board[i][j]=='R'){
-                r.xpos=i;
-                r.ypos=j;
-            }
-            else if(board[i][j]=='B'){
-                b.xpos=i;
-                b.ypos=j;
-            }
+            if(board[i][j]=='R')
+                r={i,j};
+            else if(board[i][j]=='B')
+                b={i,j};
         }
-        scanf("%c",&tmp);
     }
 
-    int iters=0xfffffff;
+    int answer=0xfffffff;
     for(int i=0;i<4;i++){
-        int num=move(N,M,r,b,1,i);
-        if(num!=-1 && num<iters)
-            iters=num;
+        int num=moveBeads(N,M,r,b,1,i);
+        if(num!=-1 && num<answer)
+            answer=num;
     }
-    if(iters==0xfffffff)
-        iters=-1;
-    cout<<iters<<endl;
+    if(answer==0xfffffff)
+        answer=-1;
+    cout<<answer<<endl;
     return 0;
 }
 void updatePosition(int &x, int &y, int xmove, int ymove){
@@ -54,18 +46,10 @@ void updatePosition(int &x, int &y, int xmove, int ymove){
     y+=ymove;
 }
 bool isEqual(int x1, int y1, int x2, int y2){
-    if(x1==x2 && y1==y2)
-        return true;
-    else
-        return false;
+    return (x1==x2) && (y1==y2);
 }
-int move(int N, int M, Position r, Position b, int iters, int direction){
+int moveBeads(int N, int M, Position r, Position b, int iters, int direction){
     if(iters>10)return -1;
- 
-    int rx=r.xpos;
-    int ry=r.ypos;
-    int bx=b.xpos;
-    int by=b.ypos;
     
     int dx[4]={-1,0,1,0};
     int dy[4]={0,1,0,-1};
@@ -75,48 +59,43 @@ int move(int N, int M, Position r, Position b, int iters, int direction){
     bool isRedInHole=false;
     bool redStop=false,blueStop=false;
     while(true){
-        if(!redStop && board[rx+xmove][ry+ymove]=='#')
+        if(!redStop && board[r.x+xmove][r.y+ymove]=='#')
             redStop=true;
-        if(!blueStop && board[bx+xmove][by+ymove]=='#')
+        if(!blueStop && board[b.x+xmove][b.y+ymove]=='#')
             blueStop=true;
         
         if(redStop && blueStop)
             break;
         else if(redStop){
-            if(isEqual(rx,ry,bx+xmove,by+ymove))
+            if(isEqual(r.x,r.y,b.x+xmove,b.y+ymove))
                 blueStop=true;
             else
-                updatePosition(bx, by, xmove, ymove);
+                updatePosition(b.x, b.y, xmove, ymove);
         }
         else if(blueStop){
-            if(isEqual(bx,by,rx+xmove,ry+ymove))
+            if(isEqual(b.x,b.y,r.x+xmove,r.y+ymove))
                 redStop=true;
             else
-                updatePosition(rx, ry, xmove, ymove);
+                updatePosition(r.x, r.y, xmove, ymove);
         }
         else{
-            updatePosition(rx, ry, xmove, ymove);
-            updatePosition(bx, by, xmove, ymove);
+            updatePosition(r.x, r.y, xmove, ymove);
+            updatePosition(b.x, b.y, xmove, ymove);
         }
-        if(board[bx][by]=='O')return -1;
-        if(isRedInHole==false && board[rx][ry]=='O'){
+        if(board[b.x][b.y]=='O')return -1;
+        if(isRedInHole==false && board[r.x][r.y]=='O'){
             isRedInHole=true;
             redStop=true;
-            rx=-1;
-            ry=-1;
+            r.x=-1;
+            r.y=-1;
         }
     }
     if(isRedInHole)return iters;
-    
-    r.xpos=rx;
-    r.ypos=ry;
-    b.xpos=bx;
-    b.ypos=by;
-    
+
     int iteration=0xfffffff;
     int nextStep[2];
-    nextStep[0]=move(N,M,r,b,iters+1,(direction+1)%4);
-    nextStep[1]=move(N,M,r,b,iters+1,(direction+3)%4);
+    nextStep[0]=moveBeads(N,M,r,b,iters+1,(direction+1)%4);
+    nextStep[1]=moveBeads(N,M,r,b,iters+1,(direction+3)%4);
     for(int i=0;i<2;i++){
         if(nextStep[i]!=-1 && iteration>nextStep[i])
             iteration=nextStep[i];
