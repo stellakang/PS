60bcf2190723632f0b00ebd123cc14e24530d5d3
kangsj123 <kangsj123@naver.com>
2020-07-29 13:16:22 +0900

    upload boj 13458

---
+++ BOJ/13458.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+long long int minimumNumOfSupervisor(int N, int arr[], int B, int C);
+int main(){
+    int N,B,C;
+    cin>>N;
+    
+    int arr[N];
+    for(int i=0;i<N;i++)
+        cin>>arr[i];
+    cin>>B>>C;
+    
+    cout<<minimumNumOfSupervisor(N,arr,B,C)<<endl;
+    return 0;
+}
+
+long long int minimumNumOfSupervisor(int N, int arr[], int B, int C){
+    long long int supervisors=0;
+    supervisors+=N;
+    for(int i=0;i<N;i++){
+        if(B>=arr[i])
+            arr[i]=0;
+        else
+            arr[i]-=B;
+        supervisors+=(arr[i]/C);
+        if(arr[i]%C>0)
+            supervisors++;
+    }
+    return supervisors;
+}
+
+
