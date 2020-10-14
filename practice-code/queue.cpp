7e30cbd1a51d03fec1dc8b226aa271cedb137004
stellakang <kangsj123@naver.com>
2020-10-14 15:23:11 +0900

    Update queue.cpp

--- practice-code/queue.cpp
+++ practice-code/queue.cpp
@@ -5,32 +5,48 @@ template<typename T>
 class Queue{
 private:
     int frontIndx,rearIndx;
+    int capacity;
     int sz;
     T* value;
 public:
     Queue(int len=10){
         frontIndx=0,rearIndx=0;
         value=new T[len];
-        sz=len;
+        capacity=len;
+        sz = 0;
     }
     ~Queue(){
         delete[] value;
     }
     void push(const T& num){
+        if(sz==capacity){
+            cout<<"error: queue is full\n";
+            exit(1);
+        }
         value[rearIndx]=num;
-        rearIndx=(rearIndx+1)%sz;
+        rearIndx=(rearIndx+1)%capacity;
+        sz++;
     }
     void pop(){
-        frontIndx=(frontIndx+1)%sz;
+        if(sz==0){
+            cout<<"error: queue is empty\n";
+            exit(1);
+        }
+        frontIndx=(frontIndx+1)%capacity;
+        sz--;
     }
     T front(){
+        if(sz==0){
+            cout<<"error: queue is empty\n";
+            exit(1);
+        }
         return value[frontIndx];
     }
     int size(){
-        return rearIndx-frontIndx;
+        return sz;
     }
     bool empty(){
-        return rearIndx-frontIndx==0;
+        return sz==0;
     }
 };
 
