bdcc2573fd2124da27a3003b744dbc5864c79c7e
kangsj123 <kangsj123@naver.com>
2021-01-11 21:25:09 +0900

    upload 73 medium

---
+++ leetcode/medium/73_set_matrix_zeroes.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        
+        vector<bool>row(m);
+        vector<bool>col(n);
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==0){
+                    row[i] = true;
+                    col[j] = true;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(row[i] || col[j])
+                    matrix[i][j] = 0;
+            }
+        }
+    }
+};
